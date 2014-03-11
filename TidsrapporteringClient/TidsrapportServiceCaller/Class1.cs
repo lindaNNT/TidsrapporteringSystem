3970b596e7b924a9571e9a4dd5d0a2cffe5bd4b2
Linda Nguyen IT-mästaren <linda_nnt@hotmail.com>
2014-03-11 15:28:00 +0100

    rollback 2

--- TidsrapporteringClient/TidsrapportServiceCaller/Class1.cs
+++ TidsrapporteringClient/TidsrapportServiceCaller/Class1.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ServiceModel;
 
 namespace TidsrapportServiceCaller
 {
     public class Class1
     {
+        TidsrapporteringService.TidsrapporteringServiceClient host = new TidsrapportServiceCaller.TidsrapporteringService.TidsrapporteringServiceClient();
+        public void test()
+        {
+            TidsrapporteringService.TidsrapporteringServiceClient host = 
+                new TidsrapportServiceCaller.TidsrapporteringService.TidsrapporteringServiceClient();
+            host.GetData();
+        }
     }
 }
