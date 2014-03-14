9996e6ec8ac04a1160981c80fd45a91e94ee3a32
Linda Nguyen IT-mästaren <linda_nnt@hotmail.com>
2014-03-14 16:00:43 +0100

    fixat dagStatus ska bara fixa metoden till den oxå. annars läs tidigare inlägg

---
+++ TidsrapporteringsSystem/TidsrapporteringsSystem/DagStatus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataLayer;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace TidsrapporteringsSystem
+{
+    [DataContract]
+    public class DagStatus
+    {
+        [DataMember]
+        public string datum { get; set; }
+
+        [DataMember]
+        public string status { get; set; }
+    }
+}
